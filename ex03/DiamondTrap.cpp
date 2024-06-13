d2b49015ad1ccc14927d0ef3aad0c7fce814fe03
Mohamad Zolfaghari Pour <mzolfagh@c3r6p2.42vienna.com>
2024-06-13 12:35:23 +0200

    minor change

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mzolfagh <mzolfagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 20:11:46 by mzolfagh          #+#    #+#             */
-/*   Updated: 2024/06/13 12:20:37 by mzolfagh         ###   ########.fr       */
+/*   Updated: 2024/06/13 12:33:17 by mzolfagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ DiamondTrap::DiamondTrap( std::string name )
     , FragTrap(name + "_clap_name")
     , _name(name)
 {
-    std::cout << "DiamondTrap " << name << " has been born!" << std::endl;  
+    std::cout << "DiamondTrap " << _name << " has been born!" << std::endl;  
     setEnergyPoint(getScavMana());
 }
 
